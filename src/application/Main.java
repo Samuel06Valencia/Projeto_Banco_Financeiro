1576ed5a2f2ab440a7ea2ef5710207dd5a7cbd1d
Samuel Silva Valencia <sv807227@gmail.com>
2023-08-27 21:38:00 -0300

    Primeira versão do Projeto

---
+++ src/application/Main.java
@@ -0,0 +1,95 @@
+package application;
+
+
+import entities.Conta;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("------------------------------");
+        System.out.println("Bem-Vindo ao Banco Financeiro!");
+        System.out.println("------------------------------");
+        System.out.println("Deseja criar uma conta conosco? (s/n)");
+        char resp = sc.next().charAt(0);
+        double dep = 0.0;
+
+        while(resp == 's') {
+            System.out.println("--------------------------------------");
+            System.out.println("Parabéns, excelente escolha!");
+            System.out.println("--------------------------------------");
+            System.out.println("Digite seu Nome: ");
+            sc.next();
+            String name = sc.nextLine();
+            System.out.println("Digite seu CPF: ");
+            long cpf = sc.nextLong();
+            System.out.println("Digite sua numeração escolhida para 'número de conta': ");
+            int numConta = sc.nextInt();
+            System.out.println("Digite sua numeração de 'número de agência': ");
+            int numAgencia = sc.nextInt();
+
+            Conta cont = new Conta(name, numConta, numAgencia, cpf);
+            System.out.println(cont.toString());
+
+
+            System.out.println("Deseja realizar seu primeiro depósito?  (s/n)");
+            resp = sc.next().charAt(0);
+
+            while(resp == 's') {
+                System.out.println("-------------------------------------");
+                System.out.println("Digite o valor de depósito desejado: ");
+                System.out.println("-------------------------------------");
+                dep = sc.nextDouble();
+
+                Conta cont1 = new Conta(name, numConta, numAgencia, cpf, dep);
+                System.out.println(cont1.toString());
+                System.out.println("Mensagem: Depósito realizado com Sucesso!");
+                break;
+            }
+            while (resp == 'n') {
+                System.out.println("---------------------");
+                System.out.println("Programa Encerrado!!!");
+                System.out.println("---------------------");
+
+            }
+
+            System.out.println("-------------------------");
+            System.out.println("Menu-Opções");
+            System.out.println("Opção 1- Depósito");
+            System.out.println("Opção 2- Saque");
+            System.out.println("-------------------------");
+            int resp1 = sc.nextInt();
+            double deposit = 0.0;
+            double saque = 0.0;
+            switch (resp1) {
+                case 1:
+                    System.out.println("Digite quanto deseja realizar de Depósito: ");
+                    deposit = sc.nextDouble();
+
+                    Conta count = new Conta(name, numConta, numAgencia, cpf, dep);
+                    count.adicionaSaldo(deposit);
+                    System.out.println(count.toString());
+                    break;
+                case 2:
+                    System.out.println("Digite quanto deseja realizar de Saque: ");
+                    saque = sc.nextDouble();
+
+                    Conta count2 = new Conta(name, numConta, numAgencia, cpf, dep);
+                    count2.adicionaSaque(saque);
+                    System.out.println(count2.totalSaque());
+                    break;
+            }
+
+            System.out.println("Programa Encerrado!!");
+
+            break;
+        }
+
+
+    }
+}
---
+++ src/entities/Conta.java
@@ -0,0 +1,102 @@
+package entities;
+
+public class Conta {
+    private String name;
+    private int numConta;
+    private int numAgencia;
+    private long cpf;
+    private double saldo;
+    private double saque;
+
+    public Conta() {
+    }
+
+    public Conta(String name, int numConta, int numAgencia, long cpf) {
+        this.name = name;
+        this.numConta = numConta;
+        this.numAgencia = numAgencia;
+        this.cpf = cpf;
+    }
+
+    public Conta(String name, int numConta, int numAgencia, long cpf, double saldo) {
+        this.name = name;
+        this.numConta = numConta;
+        this.numAgencia = numAgencia;
+        this.cpf = cpf;
+        this.saldo = saldo;
+    }
+
+    public Conta(String name, int numConta, int numAgencia, long cpf, double saldo, double saque) {
+        this.name = name;
+        this.numConta = numConta;
+        this.numAgencia = numAgencia;
+        this.cpf = cpf;
+        this.saldo = saldo;
+        this.saque = saque;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNumConta() {
+        return numConta;
+    }
+
+    public void setNumConta(int numConta) {
+        this.numConta = numConta;
+    }
+
+    public int getNumAgencia() {
+        return numAgencia;
+    }
+
+    public void setNumAgencia(int numAgencia) {
+        this.numAgencia = numAgencia;
+    }
+
+    public long getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(long cpf) {
+        this.cpf = cpf;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo += saldo;
+    }
+
+    public double getSaque() {
+        return saque;
+    }
+
+    public void setSaque(double saque) {
+        this.saque = saque;
+    }
+
+    public Double adicionaSaldo(double saldo) {
+        return this.saldo += saldo;
+    }
+    public Double adicionaSaque(double saque) {
+        return  saldo = saldo - saque;
+    }
+
+    public String toString() {
+        return "Nome: "+name+", número da Conta: "+numConta+", Agência: "+numAgencia+", Saldo Atual: "+String.format("%.3f",saldo);
+    }
+    public String totalSaldo() {
+        return "Nome: "+name+", número da Conta: "+numConta+", Agência: "+numAgencia+", Saldo Atual: "+String.format("%.3f",adicionaSaldo(saldo));
+    }
+    public String totalSaque() {
+        return "Nome: "+name+", número da Conta: "+numConta+", Agência: "+numAgencia+", Saldo Atual: "+String.format("%.3f",adicionaSaque(saque));
+    }
+}
